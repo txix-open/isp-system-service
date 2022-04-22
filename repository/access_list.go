cc94ca71a4e7e8231174f78ef3de17f3766e5067
DeniskaRediska <grj.den@gmail.com>
2022-04-22 17:02:19 +0400

    MSP-623 reimplement access_list repository

---
+++ repository/access_list.go
@@ -0,0 +1,133 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/Masterminds/squirrel"
+	"github.com/integration-system/isp-kit/db"
+	"github.com/integration-system/isp-kit/db/query"
+	"github.com/pkg/errors"
+	"isp-system-service/entity"
+)
+
+type AccessList struct {
+	db db.DB
+}
+
+func NewAccessList(db db.DB) AccessList {
+	return AccessList{
+		db: db,
+	}
+}
+
+func (r AccessList) GetAccessListByAppId(ctx context.Context, appId int) ([]entity.AccessList, error) {
+	q := `
+	SELECT app_id, method, value
+	FROM access_list
+	WHERE app_id = $1
+`
+	result := make([]entity.AccessList, 0)
+	err := r.db.Select(ctx, &result, q, appId)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "select db")
+	}
+
+	return result, nil
+}
+
+func (r AccessList) GetAccessListByAppIdList(ctx context.Context, appIdList []int) ([]entity.AccessList, error) {
+	q, args, err := query.New().
+		Select("app_id", "method", "value").
+		From("access_list").
+		Where(squirrel.Eq{"app_id": appIdList}).
+		ToSql()
+	if err != nil {
+		return nil, errors.WithMessage(err, "build query")
+	}
+
+	result := make([]entity.AccessList, 0)
+	err = r.db.Select(ctx, &result, q, args...)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "select db")
+	}
+
+	return result, nil
+}
+
+func (r AccessList) InsertArrayAccessList(ctx context.Context, entity []entity.AccessList) error {
+	qBuilder := query.New().
+		Insert("access_list").
+		Columns("app_id", "method", "value")
+	for _, e := range entity {
+		qBuilder = qBuilder.Values(e.AppId, e.Method, e.Value)
+	}
+	q, args, err := qBuilder.ToSql()
+	if err != nil {
+		return errors.WithMessage(err, "build query")
+	}
+
+	_, err = r.db.Exec(ctx, q, args...)
+	if err != nil {
+		return errors.WithMessage(err, "exec db")
+	}
+
+	return nil
+}
+
+func (r AccessList) UpsertAccessList(ctx context.Context, e entity.AccessList) (int, error) {
+	q := `
+	INSERT INTO access_list 
+	(app_id, method, value)
+	VALUES ($1, $2, $3)
+	ON CONFLICT (app_id, method) 
+		DO UPDATE
+			SET (value) = (SELECT EXCLUDED.value)
+`
+	result, err := r.db.Exec(ctx, q, e.AppId, e.Method, e.Value)
+	if err != nil {
+		return 0, errors.WithMessage(err, "exec db")
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return 0, errors.WithMessage(err, "get rows affected")
+	}
+
+	return int(rowsAffected), nil
+}
+
+func (r AccessList) DeleteAccessList(ctx context.Context, appId int, methods []string) error {
+	q, args, err := query.New().
+		Delete("access_list").
+		Where(squirrel.Eq{
+			"app_id": appId,
+			"method": methods,
+		}).
+		Suffix("RETURNING app_id, method, value").
+		ToSql()
+	if err != nil {
+		return errors.WithMessage(err, "build query")
+	}
+
+	_, err = r.db.Exec(ctx, q, args...)
+	if err != nil {
+		return errors.WithMessagef(err, "exec db")
+	}
+
+	return nil
+}
+
+func (r AccessList) DeleteAccessListByAppId(ctx context.Context, appId int) ([]entity.AccessList, error) {
+	q := `
+	DELETE FROM access_list
+	WHERE app_id = $1
+	RETURNING app_id, method, value
+`
+	result := make([]entity.AccessList, 0)
+	err := r.db.Select(ctx, &result, q, appId)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "select db")
+	}
+
+	return result, nil
+}
