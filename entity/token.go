e5d73d91de3e0073e1689715e89ca04a9cb99a1a
DeniskaRediska <grj.den@gmail.com>
2022-08-01 18:01:57 +0300

    MSP-634 add func get_auth_data to token repository

--- entity/token.go
+++ entity/token.go
@@ -10,3 +10,12 @@ type Token struct {
 	ExpireTime int
 	CreatedAt  time.Time
 }
+
+type AuthData struct {
+	SystemId   int
+	DomainId   int
+	ServiceId  int
+	AppId      int
+	ExpireTime int
+	CreatedAt  time.Time
+}
